96bd60a0299543993abf8ff18c0ba8cb7a4569a4
Justin Wehrman <jwehrman@wehr.haus>
2022-08-20 16:24:33 -0500

    Fin

--- app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -1,27 +1,35 @@
 package com.example.jetbizcard
 
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalUriHandler
 
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -87,6 +95,64 @@ fun CreateBizCard() {
 }
 
 @Composable
+private fun CreatePortfolioList() {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .padding(3.dp),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+            PorfolioItem(data = listOf("Project 1", "Project 2", "Project 3", "Project 4"))
+        }
+    }
+}
+
+@Composable
+private fun PorfolioItem(data: List<String>) {
+    LazyColumn {
+        items(data) { item ->
+            Card(
+                modifier = Modifier
+                    .padding(13.dp)
+                    .fillMaxWidth(),
+                shape = RectangleShape,
+                elevation = 4.dp,
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(7.dp)
+                        .background(MaterialTheme.colors.surface)
+                ) {
+                    CreateProfileImage(
+                        profileImage = painterResource(id = R.drawable.profile_image),
+                        description = item,
+                        modifier = Modifier
+                            .size(100.dp)
+                            .padding(end = 7.dp)
+                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(7.dp)
+                            .align(alignment = Alignment.CenterVertically)
+                    ){
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "test", style = MaterialTheme.typography.body2)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
 private fun CreateProfileImage(
     profileImage: Painter,
     description: String,
@@ -115,6 +181,10 @@ private fun CreateProfileMeta(
     occupation: String,
     githubUserName: String
 ) {
+    val portfolioToggleState = remember {
+        mutableStateOf(false)
+    }
+
     Column(modifier = modifier) {
         Text(
             text = profileName,
@@ -130,9 +200,14 @@ private fun CreateProfileMeta(
         Button(
             modifier = Modifier.align(Alignment.CenterHorizontally),
             onClick = {
-                Log.d("Clicked", "Portfolio Button Clicked")
+                portfolioToggleState.value = !portfolioToggleState.value
             }) {
             Text(text = "Portfolio", style = MaterialTheme.typography.button)
+        }
+        if (portfolioToggleState.value) {
+            CreatePortfolioList()
+        } else {
+            Box() {}
         }
     }
 }
